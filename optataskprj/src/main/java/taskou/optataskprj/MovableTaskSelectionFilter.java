fe15989b56900ff9d9398dbaa864e1d3a0e2424c
newadmin <>
2017-01-11 15:21:56 -0500

    {/optataskprj/src/main/java/taskou/optataskprj/MovableTaskSelectionFilter.java}

--- optataskprj/src/main/java/taskou/optataskprj/MovableTaskSelectionFilter.java
+++ optataskprj/src/main/java/taskou/optataskprj/MovableTaskSelectionFilter.java
@@ -2,8 +2,8 @@ package taskou.optataskprj;
 
 import org.optaplanner.core.impl.heuristic.selector.common.decorator.SelectionFilter;
 import org.optaplanner.core.impl.score.director.ScoreDirector;
-import org.optaplanner.examples.taskassigning.domain.Task;
-import org.optaplanner.examples.taskassigning.domain.TaskAssigningSolution;
+import taskou.optataskprj.Task;
+import taskou.optataskprj.TaskAssigningSolution;
 
 public class MovableTaskSelectionFilter implements SelectionFilter<TaskAssigningSolution, Task> {
 
